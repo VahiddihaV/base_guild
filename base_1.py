a5955d769ac0db56cc7554864ab1d1d1b8245ec1
VahiddihaV <111953296+VahiddihaV@users.noreply.github.com>
2026-06-25 23:40:23 +0330

    add transaction signing utility

--- base_1.py
+++ base_1.py
@@ -1,11 +1,2 @@
-def build_erc20_tx(contract, sender, to, amount, w3, chain_id):
-    return contract.functions.transfer(
-        to,
-        amount
-    ).build_transaction({
-        "from": sender,
-        "nonce": w3.eth.get_transaction_count(sender),
-        "gas": 100000,
-        "gasPrice": w3.eth.gas_price,
-        "chainId": chain_id
-    })
+def sign_transaction(w3, tx, private_key):
+    return w3.eth.account.sign_transaction(tx, private_key)
