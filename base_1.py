b4af46e3b44c6ac223604497312d2765692d7f2c
VahiddihaV <111953296+VahiddihaV@users.noreply.github.com>
2026-06-25 22:22:54 +0330

    init base toolkit project

--- base_1.py
+++ base_1.py
@@ -1,6 +1,1 @@
-def get_balance(address, w3):
-    balance = w3.eth.get_balance(address)
-    return w3.from_wei(balance, "ether")
-
-
-print(f"Balance: {get_balance(account.address, w3)} ETH")
+print("Base toolkit initialized")
