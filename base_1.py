56127205a990105bcbc30d05582e286f8f602bf9
VahiddihaV <111953296+VahiddihaV@users.noreply.github.com>
2026-06-25 23:39:13 +0330

    add erc20 transaction builder

--- base_1.py
+++ base_1.py
@@ -1,1 +1,11 @@
-# validate tx inputs
+def build_erc20_tx(contract, sender, to, amount, w3, chain_id):
+    return contract.functions.transfer(
+        to,
+        amount
+    ).build_transaction({
+        "from": sender,
+        "nonce": w3.eth.get_transaction_count(sender),
+        "gas": 100000,
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id
+    })
